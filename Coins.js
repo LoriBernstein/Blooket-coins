c34731101504e58031f94a4e0493f916aa92eb75
LoriBernstein <95389128+LoriBernstein@users.noreply.github.com>
2021-12-01 19:26:46 -0500

    Update Coins.js
    More coins

--- Coins.js
+++ Coins.js
@@ -6,11 +6,11 @@ async function getName(authToken) {
 };
 
 async function addCurrencies() {
-    const tokens = Number(prompt('How many tokens do you want to add to your account? (1000 daily)'));
+    const tokens = Number(prompt('How many tokens do you want to add to your account? (10000 daily)'));
     const myToken = localStorage.token.split('JWT ')[1];
 
-    if (tokens > 1000) {
-        alert('You can only add up to 1000 tokens daily.')
+    if (tokens > 10000) {
+        alert('You can only add up to 10000 tokens daily.')
     }
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
